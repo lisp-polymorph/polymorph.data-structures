7f39c0870dc3d133cef85a09aaf92f5b52335143
Commander thrashdin <aun.sokolov@gmail.com>
2021-07-29 11:54:12 +0300

    Indent + no utility

--- src/double-linked-list.lisp
+++ src/double-linked-list.lisp
@@ -10,106 +10,105 @@
            (dl-type  (cons 'dl-list (if (listp type) type (list type))))
            (node-code (gentemp "NODE"))
            (dl-code (gentemp "DL")))
-      `(progn
-         (eval-when (:compile-toplevel :load-toplevel :execute)
-           (defstruct ,node-code
-             (prev nil :type (or ,node-code null))
-             (data ,default :type ,type)
-             (next nil :type (or ,node-code null)))
+      `(eval-when (:compile-toplevel :load-toplevel :execute)
+         (defstruct ,node-code
+           (prev nil :type (or ,node-code null))
+           (data ,default :type ,type)
+           (next nil :type (or ,node-code null)))
 
 
-           (defstruct ,dl-code
-             (anchor (,(intern (format nil "MAKE-~s" node-code))) :type ,node-code)
-             (size 0 :type ind))
+         (defstruct ,dl-code
+           (anchor (,(intern (format nil "MAKE-~s" node-code))) :type ,node-code)
+           (size 0 :type ind))
 
-           (setf (gethash ',node-type *unparamterize-name*) ',node-code
-                 (gethash ',dl-type *unparamterize-name*) ',dl-code
-                 (gethash ',dl-code *paramterize-name*) ',dl-type
-                 (gethash ',node-code *paramterize-name*) ',node-type)
+         (setf (gethash ',node-type *unparamterize-name*) ',node-code
+               (gethash ',dl-type *unparamterize-name*) ',dl-code
+               (gethash ',dl-code *paramterize-name*) ',dl-type
+               (gethash ',node-code *paramterize-name*) ',node-type)
 
-           (defpolymorph prev ((node ,node-code)) (or ,node-code null)
-                         (,(intern (format nil "~s-PREV" node-code)) node))
-           (defpolymorph data ((node ,node-code)) ,type
-                         (,(intern (format nil "~s-DATA" node-code)) node))
-           (defpolymorph next ((node ,node-code)) (or ,node-code null)
-                         (,(intern (format nil "~s-NEXT" node-code)) node))
-           (defpolymorph (setf prev) ((new ,node-code) (node ,node-code)) ,node-code
-                         (setf (,(intern (format nil "~s-PREV" node-code)) node) new))
-           (defpolymorph (setf data) ((new ,type) (node ,node-code)) ,type
-                         (setf (,(intern (format nil "~s-DATA" node-code)) node) new))
-           (defpolymorph (setf next) ((new ,node-code) (node ,node-code)) ,node-code
-                         (setf (,(intern (format nil "~s-NEXT" node-code)) node) new))
-           (defpolymorph anchor ((container ,dl-code)) ,node-code
-                         (,(intern (format nil "~s-ANCHOR" dl-code)) container))
-           (defpolymorph size ((container ,dl-code)) ind
-                         (,(intern (format nil "~s-SIZE" dl-code)) container))
-           (defpolymorph (setf size) ((new ind) (container ,dl-code)) ind
-                         (setf (,(intern (format nil "~s-SIZE" dl-code)) container) new))
+         (defpolymorph prev ((node ,node-code)) (or ,node-code null)
+           (,(intern (format nil "~s-PREV" node-code)) node))
+         (defpolymorph data ((node ,node-code)) ,type
+           (,(intern (format nil "~s-DATA" node-code)) node))
+         (defpolymorph next ((node ,node-code)) (or ,node-code null)
+           (,(intern (format nil "~s-NEXT" node-code)) node))
+         (defpolymorph (setf prev) ((new ,node-code) (node ,node-code)) ,node-code
+           (setf (,(intern (format nil "~s-PREV" node-code)) node) new))
+         (defpolymorph (setf data) ((new ,type) (node ,node-code)) ,type
+           (setf (,(intern (format nil "~s-DATA" node-code)) node) new))
+         (defpolymorph (setf next) ((new ,node-code) (node ,node-code)) ,node-code
+           (setf (,(intern (format nil "~s-NEXT" node-code)) node) new))
+         (defpolymorph anchor ((container ,dl-code)) ,node-code
+           (,(intern (format nil "~s-ANCHOR" dl-code)) container))
+         (defpolymorph size ((container ,dl-code)) ind
+           (,(intern (format nil "~s-SIZE" dl-code)) container))
+         (defpolymorph (setf size) ((new ind) (container ,dl-code)) ind
+           (setf (,(intern (format nil "~s-SIZE" dl-code)) container) new))
 
-           (defmethod print-object ((dl ,dl-code) stream)
-             (loop :initially (format stream "#D(")
-                   :with node := (anchor dl)
-                   :for next := (next node) :then (next next)
-                   :until (eq next node)
-                   :do (format stream "~s " (data next))
-                   :finally (format stream ")")))
+         (defmethod print-object ((dl ,dl-code) stream)
+           (loop :initially (format stream "#D(")
+                 :with node := (anchor dl)
+                 :for next := (next node) :then (next next)
+                 :until (eq next node)
+                 :do (format stream "~s " (data next))
+                 :finally (format stream ")")))
 
-           (defpolymorph front ((dl-list ,dl-code)) ,type
-                         (data (next (anchor dl-list))))
+         (defpolymorph front ((dl-list ,dl-code)) ,type
+           (data (next (anchor dl-list))))
 
-           (defpolymorph back ((dl-list ,dl-code)) ,type
-                         (data (prev (anchor dl-list))))
+         (defpolymorph back ((dl-list ,dl-code)) ,type
+           (data (prev (anchor dl-list))))
 
-           (defpolymorph (setf front) ((new ,type) (dl-list ,dl-code)) ,type
-                         (setf (data (next (anchor dl-list))) new))
+         (defpolymorph (setf front) ((new ,type) (dl-list ,dl-code)) ,type
+           (setf (data (next (anchor dl-list))) new))
 
-           (defpolymorph (setf back) ((new ,type) (dl-list ,dl-code)) ,type
-                         (setf (data (prev (anchor dl-list))) new))
+         (defpolymorph (setf back) ((new ,type) (dl-list ,dl-code)) ,type
+           (setf (data (prev (anchor dl-list))) new))
 
-           (defpolymorph empty-p ((dl-list ,dl-code)) boolean
-                         (= 0 (size dl-list)))
+         (defpolymorph empty-p ((dl-list ,dl-code)) boolean
+           (= 0 (size dl-list)))
 
-           (defpolymorph push-front ((data ,type) (dl-list ,dl-code)) ,type
-                         (let* ((anchor (anchor dl-list))
-                                (current (next anchor))
-                                (new-node (,(intern (format nil "MAKE-~s" node-code))
-                                            :data data :next current)))
-                           (setf (prev current) new-node
-                                 (next anchor) new-node)
-                           (incf (size dl-list))
-                           data))
+         (defpolymorph push-front ((data ,type) (dl-list ,dl-code)) ,type
+           (let* ((anchor (anchor dl-list))
+                  (current (next anchor))
+                  (new-node (,(intern (format nil "MAKE-~s" node-code)))))
+                 :data data :next current)))
+       (setf (prev current) new-node
+             (next anchor) new-node)
+       (incf (size dl-list))
+       data
 
-           (defpolymorph push-back ((data ,type) (dl-list ,dl-code)) ,type
-                         (let* ((anchor (anchor dl-list))
-                                (current (prev anchor))
-                                (new-node (,(intern (format nil "MAKE-~s" node-code))
-                                            :data data :prev current)))
-                           (setf (next current) new-node
-                                 (prev anchor) new-node)
-                           (incf (size dl-list))
-                           data))
+       (defpolymorph push-back ((data ,type) (dl-list ,dl-code)) ,type
+         (let* ((anchor (anchor dl-list))
+                (current (prev anchor))
+                (new-node (,(intern (format nil "MAKE-~s" node-code)))))
+               :data data :prev current
+           (setf (next current) new-node
+                 (prev anchor) new-node)
+           (incf (size dl-list))
+           data))
 
-           (defpolymorph pop-front ((dl-list ,dl-code)) ,type
-                         (if (= 0 (size dl-list))
-                             (error 'simple-error :format-control "Cannot pop from an empty list")
-                             (let* ((anchor (anchor dl-list))
-                                    (current (next anchor))
-                                    (data (data current)))
-                               (setf (prev (next current)) nil
-                                     (next anchor) (next current))
-                               (decf (size dl-list))
-                               data)))
+       (defpolymorph pop-front ((dl-list ,dl-code)) ,type
+         (if (= 0 (size dl-list))
+             (error 'simple-error :format-control "Cannot pop from an empty list")
+             (let* ((anchor (anchor dl-list))
+                    (current (next anchor))
+                    (data (data current)))
+               (setf (prev (next current)) nil
+                     (next anchor) (next current))
+               (decf (size dl-list))
+               data)))
 
-           (defpolymorph pop-back ((dl-list ,dl-code)) ,type
-                         (if (= 0 (size dl-list))
-                             (error 'simple-error :format-control "Cannot pop from an empty list")
-                             (let* ((anchor (anchor dl-list))
-                                    (current (prev anchor))
-                                    (data (data current)))
-                               (setf (next (prev current)) nil
-                                     (prev anchor) (prev current))
-                               (decf (size dl-list))
-                               data))))))))
+       (defpolymorph pop-back ((dl-list ,dl-code)) ,type
+         (if (= 0 (size dl-list))
+             (error 'simple-error :format-control "Cannot pop from an empty list")
+             (let* ((anchor (anchor dl-list))
+                    (current (prev anchor))
+                    (data (data current)))
+               (setf (next (prev current)) nil
+                     (prev anchor) (prev current))
+               (decf (size dl-list))
+               data))))))
 
 
 
