d45f61ec15a2f68009bafe116c0149b7b98f6580
Plisp <tianlinqu@gmail.com>
2021-10-02 12:57:30 +1000

    ring-buffer fix

--- src/ring-buffer.lisp
+++ src/ring-buffer.lisp
@@ -11,8 +11,7 @@
 
       `(eval-when (:compile-toplevel :load-toplevel :execute)
          (defstruct ,buf-code
-           (begin 0 :type ind)
-           (end 0 :type ind)
+           (begin 0 :type ind) ; index [0, size-1]
            (size 0 :type ind)
            (data (make-array 0 :element-type ',type :initial-element ,default)
             :type (simple-array ,type (cl:*))))
@@ -24,10 +23,6 @@
            (,(intern (format nil "~s-BEGIN" buf-code)) buf))
          (defpolymorph (setf begin) ((new ind) (buf ,buf-code)) ind
            (setf (,(intern (format nil "~s-BEGIN" buf-code)) buf) new))
-         (defpolymorph end ((buf ,buf-code)) (values ind &optional)
-           (,(intern (format nil "~s-END" buf-code)) buf))
-         (defpolymorph (setf end) ((new ind) (buf ,buf-code)) ind
-           (setf (,(intern (format nil "~s-END" buf-code)) buf) new))
          (defpolymorph size ((buf ,buf-code)) (values ind &optional)
            (,(intern (format nil "~s-SIZE" buf-code)) buf))
          (defpolymorph (setf size) ((new ind) (buf ,buf-code)) ind
@@ -44,99 +39,79 @@
            (let ((newdata (make-array newsize :element-type ',type
                                               :initial-element ,default))
                  (olddata (data buf))
-                 (front (begin buf))
-                 (back (end buf)))
-             (if (< front back)
-                 (progn (loop :for i :from front :below back
-                              :do (setf (aref newdata (- i front)) (aref olddata i)))
-                        (setf (begin buf) 0
-                              (end buf) (if (= newsize (- back front))
-                                            0
-                                            (- back front))))
-                 (progn (loop :for i :from 0 :below back
-                              :do (setf (aref newdata i) (aref olddata i)))
-                        (loop :for i :from front :below (length olddata)
-                              :do (setf (aref newdata (+ i (- newsize (length olddata))))
-                                        (aref olddata i)))
-                        (setf (begin buf) (the ind
-                                               (- (begin buf)
-                                                  (- newsize (length olddata)))))))
-             (setf (data buf) newdata)
-             nil))
-
-
-         (defpolymorph front ((buf ,buf-code)) ,type
-           (if (= 0 (size buf))
-               (error "Front requires a non-empty biffer")
-               (aref (data buf) (begin buf))))
-
-         (defpolymorph (setf front) ((new ,type) (buf ,buf-code)) ,type
-           (if (= 0 (size buf))
-               (error "Front requires a non-empty biffer")
-               (setf (aref (data buf) (begin buf)) new)))
-
-         (defpolymorph push-front ((new ,type) (buf ,buf-code)) ,type
-           (when (= (length (data buf)) (size buf))
-             (%resize buf (the ind (* 2 (+ 1 (length (data buf)))))))
-           (setf (begin buf)
-                 (the ind (if (= 0 (begin buf))
-                              (- (length (data buf)) 1)
-                              (- (begin buf) 1))))
-           (setf (aref (data buf) (begin buf)) new)
-           (setf (size buf) (the ind (1+ (size buf))))
-           new)
-
-         (defpolymorph pop-front ((buf ,buf-code)) ,type
-           (if (= 0 (size buf))
-               (error "Front requires a non-empty buffer")
-               (prog1
-                   (aref (data buf) (begin buf))
-                 (when (> (length (data buf)) (* 3 (size buf)))
-                   (%resize buf (size buf)))
-                 (setf (begin buf)
-                       (the ind (if (= (1- (length (data buf))) (begin buf))
-                                    0
-                                    (1+ (begin buf)))))
-                 (setf (size buf) (the ind (1- (size buf)))))))
+                 (begin (begin buf)))
+             (if (= (length olddata) 0)
+                 (setf (data buf) newdata)
+                 (loop :for i :below (size buf)
+                       :do (setf (aref newdata i)
+                                 (aref olddata (mod (+ begin i) (length olddata))))
+                       :finally (setf (begin buf) 0
+                                      (data buf) newdata)))))
 
 
          (defpolymorph back ((buf ,buf-code)) ,type
            (if (= 0 (size buf))
-               (error "Back requires a non-empty buffer")
-               (aref (data buf)
-                     (mod (1- (begin buf)) (size buf)))))
+               (error "BACK requires a non-empty buffer")
+               (aref (data buf) (mod (1- (+ (begin buf) (size buf)))
+                                     (length (data buf))))))
 
          (defpolymorph (setf back) ((new ,type) (buf ,buf-code)) ,type
            (if (= 0 (size buf))
-               (error "Back requires a non-empty buffer")
-               (setf (aref (data buf)
-                           (mod (1- (begin buf)) (size buf)))
+               (error "BACK requires a non-empty buffer")
+               (setf (aref (data buf) (mod (1- (+ (begin buf) (size buf)))
+                                           (length (data buf))))
                      new)))
 
          (defpolymorph push-back ((new ,type) (buf ,buf-code)) ,type
            (when (= (length (data buf)) (size buf))
-             (%resize buf (* 2 (+ 1 (length (data buf))))))
-           (setf (aref (data buf) (end buf)) new)
-           (setf (end buf)
-                 (the ind (if (= (1- (length (data buf))) (end buf))
-                              0
-                              (1+ (end buf)))))
+             (%resize buf (the ind (* 2 (+ 1 (length (data buf)))))))
+           (setf (aref (data buf) (mod (+ (begin buf) (size buf))
+                                       (length (data buf))))
+                 new)
            (setf (size buf) (the ind (1+ (size buf))))
            new)
 
-
          (defpolymorph pop-back ((buf ,buf-code)) ,type
            (if (= 0 (size buf))
-               (error "Back requires a non-empty buffer")
-               (progn
+               (error "POP-BACK requires a non-empty buffer")
+               (prog1
+                   (front buf)
                  (when (> (length (data buf)) (* 3 (size buf)))
                    (%resize buf (size buf)))
-                 (setf (end buf)
-                       (the ind (if (= 0 (end buf))
-                                    (1- (length (data buf)))
-                                    (1- (end buf)))))
-                 (setf (size buf) (the ind (1- (size buf))))
-                 (aref (data buf) (end buf)))))
+                 (setf (size buf) (the ind (1- (size buf)))))))
+
+
+         (defpolymorph front ((buf ,buf-code)) ,type
+           (if (= 0 (size buf))
+               (error "FRONT requires a non-empty buffer")
+               (aref (data buf) (begin buf))))
+
+         (defpolymorph (setf front) ((new ,type) (buf ,buf-code)) ,type
+           (if (= 0 (size buf))
+               (error "FRONT requires a non-empty buffer")
+               (setf (aref (data buf) (begin buf))
+                     new)))
+
+         (defpolymorph push-front ((new ,type) (buf ,buf-code)) ,type
+           (when (= (length (data buf)) (size buf))
+             (%resize buf (* 2 (+ 1 (length (data buf))))))
+           (setf (begin buf) (mod (1- (begin buf))
+                                  (length (data buf)))
+                 (aref (data buf) (begin buf)) new
+                 (size buf) (the ind (1+ (size buf))))
+           new)
+
+
+         (defpolymorph pop-front ((buf ,buf-code)) ,type
+           (if (= 0 (size buf))
+               (error "POP-FRONT requires a non-empty buffer")
+               (prog1
+                   (back buf)
+                 (when (> (length (data buf)) (* 3 (size buf)))
+                   (%resize buf (size buf)))
+                 (setf (size buf) (the ind (1- (size buf)))
+                       (begin buf) (mod (1+ (begin buf))
+                                        (length (data buf)))))))
 
          (defpolymorph empty-p ((buf ,buf-code)) boolean
            (= 0 (size buf)))
@@ -152,9 +127,7 @@
                                indexes))
                   (ind (first indexes)))
              (flet ((%at ()
-                      (aref data (if (> ind (+ begin (length data)))
-                                     (+ begin ind (- (length data)))
-                                     (+ begin ind)))))
+                      (aref data (mod (+ begin ind) (length data)))))
                (declare (inline %at))
                (if error-policy
 
@@ -162,7 +135,7 @@
                        (progn
                          (unless (= 1 (length indexes))
                            (error 'simple-error :format-control "Only one index is allowed for ring-buffer")) ;; FIXME later make this better
-                         (unless (< (+ begin ind) size)
+                         (unless (< ind size)
                            (error 'simple-error
                                    :format-control "Invalid index ~s for (RING-BUFFER ~s), should be a non-negative integer below ~s."
                                    :format-arguments (list ind size size)))
@@ -174,7 +147,7 @@
                    (progn
                      (unless (= 1 (length indexes))
                        (error 'simple-error :format-control "Only one index is allowed for ring-buffer")) ;; FIXME later make this better
-                     (unless (< (+ begin (first indexes)) size)
+                     (unless (< (first indexes) size)
                        (error 'simple-error
                                :format-control "Invalid index ~s for (RING-BUFFER ~s), should be a non-negative integer below ~s."
                                :format-arguments (list (first indexes) size size)))
@@ -194,9 +167,7 @@
                                indexes))
                   (ind (first indexes)))
              (flet ((set-at ()
-                      (setf (aref data (if (> ind (+ begin (length data)))
-                                           (+ begin ind (- (length data)))
-                                           (+ begin ind)))
+                      (setf (aref data (mod (+ begin ind) (length data)))
                             new)))
                (declare (inline set-at))
                (if error-policy
@@ -205,7 +176,7 @@
                        (progn
                          (unless (= 1 (length indexes))
                            (error 'simple-error :format-control "Only one index is allowed for ring-buffer")) ;; FIXME later make this better
-                         (unless (< (+ begin ind) size)
+                         (unless (< ind size)
                            (error 'simple-error
                                    :format-control "Invalid index ~s for (RING-BUFFER ~s), should be a non-negative integer below ~s."
                                    :format-arguments (list ind size size)))
@@ -217,7 +188,7 @@
                    (progn
                      (unless (= 1 (length indexes))
                        (error 'simple-error :format-control "Only one index is allowed for ring-buffer")) ;; FIXME later make this better
-                     (unless (< (+ begin (first indexes)) size)
+                     (unless (< (first indexes) size)
                        (error 'simple-error
                                :format-control "Invalid index ~s for (RING-BUFFER ~s), should be a non-negative integer below ~s."
                                :format-arguments (list (first indexes) size size)))
@@ -266,14 +237,22 @@
 (defun ring-buffer-adhoc-test ()
   (let ((b (ring-buffer 'fixnum)))
     (push-front 1 b)
+    (print b)
     (push-front 2 b)
+    (print b)
     (push-front 3 b)
+    (print b)
     (push-front 4 b)
+    (print b)
     (pop-front b)
+    (print b)
     (pop-back b)
-    (push-back 1 b)
+    (print b)
+    (push-back 5 b)
+    (print b)
     (push-back 99 b)
+    (print b)
     (assert (and (= (at b 0) 3)
                  (= (at b 1) 2)
-                 (= (at b 2) 1)
+                 (= (at b 2) 5)
                  (= (at b 3) 99)))))
